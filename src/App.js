d31f4ab0b64f537527402ef0617358ef46972b5d
blc132 <pawelkob97@wp.pl>
2018-04-04 23:10:26 +0200

    connecting frontend with backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'; //react import
 import Board from './components/Board' //import boarda
 import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import NewBoard from './components/NewBoard';
 
 class App extends Component {
   render() {
-    return (<div>
-      <Board/>
+    return (
+    <div className="App">
+    <BrowserRouter>
+    <div>
+      <Route path='/new' component={NewBoard}/>
+      <Route exact path='/' component={Board}/>
+    </div>
+    </BrowserRouter>
     </div>);
   }
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,44 +2,61 @@ import React from 'react';
 import List from './List' //import listy
 import Card from './Card' //import card
 import FakeData from '../FakeData';
+import { withRouter } from 'react-router-dom';
+import { BASE_URL } from '../constans';
+import axios from 'axios';
 import './Board.css'
 
-export default class Board extends React.Component {
+
+export class Board extends React.Component {
     constructor() {
         super() //wywolujemy konstruktor React.Component
         this.state = {
-            boardName: 'Best board ever!',
+            boardName: '',
+            boardId: 0,
             boardData: [],
         }
     }
 
     componentDidMount() {
-        this.setState({boardData: FakeData}) //jedyny sposób na ustawienie naszego stanu po za momentem inicjalizacji
+        axios.get(BASE_URL + '/board').then((response) => {
+            console.log(response);
+            this.setState({
+                boardData: response.data.lists,
+                boardName: response.data.name,
+                boardId: response.data.id
+            })
+
+            }).catch(() => {
+              this.props.history.push('/new'); //jedyny sposób na ustawienie naszego stanu po za momentem inicjalizacji
+        });
     }
 
     renderList = () => {
         console.log(this.state.boardData)
-        return(
-             this.state.boardData.map(
-            (list) => <List listName={list.listName} cards={list.cards} />)
-            )
+        return (
+            this.state.boardData.map(
+                (list) => <List listName={list.name} cards={[]} />)
+        )
     }
 
 
     render() {
-        return(
+        return (
             <div>
                 <div>
                     {this.state.boardName}
                     <button className="btn btn-info">Add new list</button>
-                    <input/>
+                    <input />
                 </div>
-                    <div className="container-fluid">
-                        <div className="row flex-row flex-nowrap">
+                <div className="container-fluid">
+                    <div className="row flex-row flex-nowrap">
                         {this.renderList()}
-                        </div>
                     </div>
                 </div>
+            </div>
         )
     }
 }
+
+export default withRouter(Board)
