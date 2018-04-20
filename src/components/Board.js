c8841d62e4b1db64e5d2d9361b8bb8ad55bd7724
blc132 <pawelkob97@wp.pl>
2018-04-20 12:26:01 +0200

    done: adding cards, todo: deletinng cards and lists

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,6 @@ import List from './List' //import listy
 import Card from './Card' //import card
 import FakeData from '../FakeData';
 //gdy nie ma export default to nie uzywamy nawiasów
-
 import { withRouter } from 'react-router-dom';
 import { BASE_URL } from '../constans';
 import axios from 'axios';
@@ -21,7 +20,7 @@ class Board extends React.Component {
             cards: [],
         }
     }
-//wykona sie po renderze komponentu
+    //wykona sie po renderze komponentu
     componentDidMount() {
         axios.get(BASE_URL + '/board').then((response) => {
             console.log(response);
@@ -31,8 +30,8 @@ class Board extends React.Component {
                 boardId: response.data.id
             })
 
-            }).catch(() => {
-              this.props.history.push('/new'); //jedyny sposób na ustawienie naszego stanu po za momentem inicjalizacji
+        }).catch(() => {
+            this.props.history.push('/new'); //jedyny sposób na ustawienie naszego stanu po za momentem inicjalizacji
         });
     }
 
@@ -41,27 +40,27 @@ class Board extends React.Component {
     }
 
     onClickList = (e) => {
-        axios.post(BASE_URL + "/list",{BoardId: this.state.boardId,Name: this.state.listName})
-        .then(response => {
-            console.log(response);
-            this.setState(prevState => {
-                return {
-                    //te trzy kropki to taki operator ktory wezmie wszystkie komponenty, tworzymy nowa tablice
-                    boardData: [...prevState.boardData, response.data],
-                    listName: ''
-                }
-            });
-        })
-       //console.log(this.state.listName)
+        axios.post(BASE_URL + "/list", { BoardId: this.state.boardId, Name: this.state.listName })
+            .then(response => {
+                console.log(response);
+                this.setState(prevState => {
+                    return {
+                        //te trzy kropki to taki operator ktory wezmie wszystkie komponenty, tworzymy nowa tablice
+                        boardData: [...prevState.boardData, response.data],
+                        listName: ''
+                    }
+                });
+            })
+        //console.log(this.state.listName)
     }
 
     renderLists = () => {
         console.log(this.state.boardData)
         return (
             this.state.boardData.map((list) =>
-            // react bedzie przeladowywal tylko te komponenty ktore zostaly zmienione - key
-             <List key={list.id} boardId={this.state.boardId} listId={list.id}
-              listName={list.name} cards={[]} />
+                // react bedzie przeladowywal tylko te komponenty ktore zostaly zmienione - key
+                <List key={list.id} boardId={this.state.boardId} listId={list.id}
+                    listName={list.name} cards={list.cards} />
             ));
     };
 
@@ -69,15 +68,15 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-        <div>
-          <h1>{this.state.boardName}</h1>
-          <button className="btn btn-info" onClick={this.onClickList} disabled={!this.state.listName}>Add new list</button>
-          <input type="text" value={this.state.listName} onChange={this.onChangeListName} className="listName__input" />
-        </div>
-        <div className="container-fluid">
-          <div className="row flex-row flex-nowrap">{this.renderLists()}</div>
-        </div>
-      </div>
+                <div>
+                    <h1>{this.state.boardName}</h1>
+                    <button className="btn btn-info" onClick={this.onClickList} disabled={!this.state.listName}>Add new list</button>
+                    <input type="text" value={this.state.listName} onChange={this.onChangeListName} className="listName__input" />
+                </div>
+                <div className="container-fluid">
+                    <div className="row flex-row flex-nowrap">{this.renderLists()}</div>
+                </div>
+            </div>
         )
     }
 }
