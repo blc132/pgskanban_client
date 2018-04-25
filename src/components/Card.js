ddcd76215f73e5dc73c0531d3a68bca73a46ec12
blc132 <pawelkob97@wp.pl>
2018-04-25 22:52:27 +0200

    last workshops, details

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import './Card.css'
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '500px',
+    }
+};
 
 export default class Card extends Component {
 
+    constructor() {
+        super();
 
-    render() {
-        return(
-            this.newMethod(),
-            <div className="card__container">{this.props.cardName}</div>
-        )
+        this.state = {
+            modalIsOpen: false
+        };
     }
 
-    newMethod() {
-        return <button className="btn btn-warning btn-sm">Card</button>;
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    }
+
+    afterOpenModal = () => {
+        // references are now sync'd and can be accessed.
+       // this.subtitle.style.color = '#f00';
+    }
+
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
+
+    showDescription = ()  => {
+        
+    }
+
+    render() {
+        return (
+            <div onClick={this.showDescription} className="card__container">
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2 ref={subtitle => this.subtitle}>{this.props.card}</h2>
+                    <dic className="form-group">
+                        <label for="description">Description:</label>
+                        <textarea className="form-control" rows="5" id="description"></textarea>
+                        <button onClick={this.closeModal} className="btn btn-danger col-2">X</button>
+                        <button className="btn btn-info">Save</button>
+                    </dic>
+                </Modal>
+                <div className="row">
+                    <div onClick={this.openModal} className="col-10">{this.props.cardName}</div>
+                    <button className="btn btn-danger btn-sm">X</button>
+                </div>
+            </div>
+
+        )
     }
 }
--- src/components/List.js
+++ src/components/List.js
@@ -10,7 +10,9 @@ export default class List extends Component {
         this.state = {
             id: props.listId,
             name: props.listName,
-            cardName: "",
+            cards: props.cards,
+            cardName: [],
+            newCardName: ''
         };
     }
 
@@ -21,19 +23,27 @@ export default class List extends Component {
     }
 
     addCard = (e) => {
-        axios.post(BASE_URL + '/card', { Name: this.state.cardName, ListId: this.state.id}
-    ).then(() => {
-        console.log("Successfully updated card!")
-    }).catch((error) => {
-        console.log(this.state.cardName);
-        console.log(this.state.id);
-        console.log(error);
-    });
+        axios.post(BASE_URL + '/card', { Name: this.state.cardName, ListId: this.props.id }
+        ).then((res) => {
+            this.setState(prevState => {
+                return {
+                    cards: [...prevState.cards, { listId: this.props.listId, name: this.state.cardName}],
+                    newCardName: ''
+                }
+            })
+            console.log("Successfully updated card!")
+        }).catch((error) => {
+            console.log(this.state.cardName);
+            console.log(this.state.id);
+            console.log(error);
+        });
+
+
     }
 
     onCardNameChange = e => {
         console.log(this.state.cardName);
-        this.setState({ cardName: e.target.value });
+        this.setState({ newCardName: e.target.value });
     }
 
     onListNameChange = e => {
@@ -41,12 +51,28 @@ export default class List extends Component {
     }
 
     saveListName = () => {
-        axios.put(BASE_URL + '/list', { name: this.state.name, boardId: this.props.boardId, listId: this.props.listId }
+        axios.put(BASE_URL + '/list', { name: this.state.name, boardId: this.props.boardId, listId: this.props.id }
         ).then(() => {
             console.log("Successfully updated list!")
         }).catch((error) => {
             console.log(error);
         });
+    }
+
+    deleteList = () => {
+        console.log("board id: " + this.state.boardId + "list name: " +  this.state.listName + "list id: " +  this.state.listId)
+        axios.delete(BASE_URL + "/list", { boardId: this.state.boardId, Name: this.state.listName, listId: this.state.listId })
+            .then(response => {
+                console.log(response);
+                this.setState(prevState => {
+                    return {
+                        //te trzy kropki to taki operator ktory wezmie wszystkie komponenty, tworzymy nowa tablice
+                        boardData: [...prevState.boardData, response.data],
+                        listName: ''
+                    }
+                });
+            })
+        //console.log(this.state.listName)
     }
 
     render() {
@@ -55,7 +81,7 @@ export default class List extends Component {
                 <div className="row">
                     <input value={this.state.name} onChange={this.onListNameChange} className="form-control col-8" />
                     <button onClick={this.saveListName} className="btn btn-success col-2">Edit</button>
-                    <button className="btn btn-danger col-2 btn-block">X</button>
+                    <button onClick={this.deleteList} className="btn btn-danger col-2 btn-block">X</button>
                 </div>
                 <div className="card card-block">
                     {this.renderCards()}
